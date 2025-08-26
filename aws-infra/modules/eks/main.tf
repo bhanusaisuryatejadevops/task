75056155712b50436906f3eb620e9853dec87073
surya <surya@gmail.com>
2025-08-26 12:37:11 +0530

    updates

--- aws-infra/modules/eks/main.tf
+++ aws-infra/modules/eks/main.tf
@@ -4,6 +4,9 @@ variable "subnets" {
 }
 variable "ecr_repo_url" {}
 
+# -----------------------------
+# IAM Role for EKS Cluster
+# -----------------------------
 data "aws_iam_policy_document" "eks_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -19,6 +22,53 @@ resource "aws_iam_role" "eks_cluster_role" {
   assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
 
+# Attach required policies to cluster role
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# -----------------------------
+# IAM Role for Node Group
+# -----------------------------
+data "aws_iam_policy_document" "node_group_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name               = "eks-node-group-role"
+  assume_role_policy = data.aws_iam_policy_document.node_group_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# -----------------------------
+# EKS Cluster
+# -----------------------------
 resource "aws_eks_cluster" "this" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
@@ -28,10 +78,13 @@ resource "aws_eks_cluster" "this" {
   }
 }
 
+# -----------------------------
+# EKS Node Group
+# -----------------------------
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "my-eks-nodes"
-  node_role_arn   = aws_iam_role.eks_cluster_role.arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.subnets
 
   scaling_config {
@@ -43,6 +96,9 @@ resource "aws_eks_node_group" "node_group" {
   instance_types = ["t3.medium"]
 }
 
+# -----------------------------
+# Outputs
+# -----------------------------
 output "cluster_name" {
   value = aws_eks_cluster.this.name
 }
