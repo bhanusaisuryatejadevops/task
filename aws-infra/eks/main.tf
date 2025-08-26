ef450fe3c6233f2118813a3fe2e9241571867337
surya <surya@gmail.com>
2025-08-26 11:09:24 +0530

    updates

--- aws-infra/eks/main.tf
+++ aws-infra/eks/main.tf
@@ -1,3 +1,7 @@
+# -----------------------------------
+# Existing EKS Cluster + Node Group
+# -----------------------------------
+
 variable "vpc_id" {
   type = string
 }
@@ -10,28 +14,64 @@ variable "eks_role_arn" {
   type = string
 }
 
+# Optional Node Group variables
+variable "node_group_name" {
+  type    = string
+  default = "main-node-group"
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "max_capacity" {
+  type    = number
+  default = 3
+}
+
+variable "min_capacity" {
+  type    = number
+  default = 1
+}
+
+# ----------------------------
+# Security Group for EKS
+# ----------------------------
 resource "aws_security_group" "eks" {
   vpc_id = var.vpc_id
+
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "eks-sg"
   }
 }
 
+# ----------------------------
+# EKS Cluster
+# ----------------------------
 resource "aws_eks_cluster" "main" {
   name     = "main-cluster"
   role_arn = var.eks_role_arn
+
   vpc_config {
     subnet_ids         = var.subnet_ids
     security_group_ids = [aws_security_group.eks.id]
@@ -39,6 +79,38 @@ resource "aws_eks_cluster" "main" {
   }
 }
 
+# ----------------------------
+# EKS Node Group
+# ----------------------------
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = var.node_group_name
+  node_role_arn   = var.eks_role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_capacity
+    min_size     = var.min_capacity
+  }
+
+  instance_types = [var.node_instance_type]
+  ami_type       = "AL2_x86_64"
+
+  tags = {
+    Name        = var.node_group_name
+    Environment = "dev"
+    Project     = "CRPIT"
+  }
+}
+
+# ----------------------------
+# Outputs
+# ----------------------------
 output "cluster_name" {
   value = aws_eks_cluster.main.name
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.main.node_group_name
 }
